4abb431609ce9fdf938dd34806ed0e944419214b
Oriane Cosson <oriane.cosson@accenture.com>
2026-09-10 16:52:46 +0200

    Remove obsolete ImageTarget and add the fullcard version

--- ardemo-studio/src/app.js
+++ ardemo-studio/src/app.js
@@ -1,11 +1,7 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../image-targets/ImageTarget_faceB.json'),
-      require('../image-targets/ImageTarget_faceB_v2.json'),
-      require('../image-targets/ImageTarget_faceB_photo.json'),
-      require('../image-targets/DrawingTarget.json'),
-      require('../image-targets/ImageTarget.json')
+      require('../image-targets/ImageTarget_fullcard.json')
     ],
   })
 }
